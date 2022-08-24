0e921cf2430d783783c846d36a69fef257889f78
Filipe Anjos <110917838+Anjosfilipe@users.noreply.github.com>
2022-08-24 19:11:10 -0300

    Adicionar arquivos de projeto.

---
+++ PObjeto_Aluno_funcionario/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PObjeto_Aluno_funcionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
